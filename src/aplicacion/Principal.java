646b225fd498dbeafa7f237673cdbb09844339e3
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:09:20 +0100

    ej3 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,15 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_3;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int base;
+        int exponente;
+        System.out.println("Introduce una base");
+        base=sc.nextInt();
+        System.out.println("Introduce un exponente");
+        exponente=sc.nextInt();
+        System.out.println("La potencia de "+base+" elevado a "+exponente+ " es " +ej2_3.potencia(base,exponente));
+    }
 }
--- src/dominio/ej2_3.java
+++ src/dominio/ej2_3.java
@@ -1,2 +1,18 @@
-package dominio;public class ej2_3 {
+package dominio;
+
+public class ej2_3 {
+
+
+
+    public static int  potencia(int  base,int exponente) {
+
+        if( exponente==0) {
+            return 1;
+
+        }else {
+            return (base)*potencia(base,exponente-1);
+
+        }
+    }
+
 }
